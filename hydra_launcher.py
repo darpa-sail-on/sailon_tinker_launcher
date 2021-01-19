943955af7947fe5bbd939604c8fcfc0efe06d866
chris <christopher.funk@kitware.com>
2021-01-19 16:56:59 -0500

    Updated configs to be more sensible.  Added multirun and slurm info for hydra laucher.  Moved hydra launcher to root dir to make output more sensible (and since it shouldn't be called anyway).  Deleted some useless files as well

--- hydra_launcher.py
+++ hydra_launcher.py
@@ -8,7 +8,7 @@ log = logging.getLogger(__name__)
 
 @hydra.main(config_path="configs", config_name="h_config")
 def hydra_launcher(cfg: DictConfig) -> None:
-    """ Load the protocol and then run it
+    """ Load the protocol and then run it.  Check README.md for how to run.
 
     """
     log.info(cfg)
